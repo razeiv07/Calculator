961c49eb3769bc643d7f86caacc08f9ce51a662e
razeiv07 <rajeevxrma@gmail.com>
2024-07-10 19:22:28 -0400

    Initial Commit

---
+++ Simple Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simple Calculator
+//
+//  Created by Rajeev Sharma on 2024-07-10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
